6b08b74368d3212f102385e61448c7bfbf7ac4ed
Etumnu Prosper <johnikechukwu94@gmail.com>
2023-10-08 14:59:36 +0100

    Fixed blog

--- blog-app-client/components/home/BlogItem.tsx
+++ blog-app-client/components/home/BlogItem.tsx
@@ -2,6 +2,9 @@ import { BlogType } from "@/types";
 import Link from "next/link";
 
 const BlogItem = ({ post }: { post: BlogType }) => {
+  //regex to extract the text from the htmlcontent
+  const textContent = post.content.replace(/<[^>]*>/g, "");
+  const trimmedText = textContent.substring(0, 62) + "...";
   return (
     <Link href={`/blog/${post.slug}`}>
       <div className="md:w-64 w-80 mb-4 md:mb-6 h-auto  ">
@@ -17,9 +20,16 @@ const BlogItem = ({ post }: { post: BlogType }) => {
           {new Date(post.created_at).toDateString()}
         </div>
         <div className="border-b-4 my-5 border-primary w-32 " />
-        <p className="text-textcolor  w-full text-sm">
-          {post.content.substring(10, 80) + "..."}
-        </p>
+        <span>
+          {" "}
+          {post.content[0] === "<" ? (
+            <p className="text-textcolor  w-full text-sm">{trimmedText}</p>
+          ) : (
+            <p className="text-textcolor  w-full text-sm">
+              {post.content.substring(0, 67) + "..."}
+            </p>
+          )}
+        </span>
       </div>
     </Link>
   );
